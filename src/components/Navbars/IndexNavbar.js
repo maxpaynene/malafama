6f365bbcc218cc13809dcc72b24eb03e0e7d7380
Marcelo Vilches <marcelo.vilches@cencosud.cl>
2021-09-13 19:52:47 -0300

    worcking in progress

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -70,11 +70,13 @@ export default function IndexNavbar() {
   const onCollapseExited = () => {
     setCollapseOut("");
   };
+  /*
   const scrollToDownload = () => {
     document
       .getElementById("download-section")
       .scrollIntoView({ behavior: "smooth" });
   };
+  */
   return (
     <Navbar className={"fixed-top " + color} color-on-scroll="100" expand="lg">
       <Container>
